4ef826ae685d5efc50c125c0b8cc2ddbbb05046b
Elizaveta Biryukova <birliza@mail.ru>
2022-07-25 12:53:43 +0200

    Adds anchor links

--- js/main.js
+++ js/main.js
@@ -2,12 +2,29 @@ const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.nav-button');
 const navBtnImg = document.querySelector('.nav-button-img');
 
+const navLink = document.querySelector('.nav-link');
+const navLink1 = document.querySelector('.nav-link--1');
+const navLink2 = document.querySelector('.nav-link--2');
+const navLink3 = document.querySelector('.nav-link--3');
+// const home = document.querySelector('.header');
+// const portfolio = document.querySelector('.portfolio');
+// const contacts = document.querySelector('.contacts');
+// const SCROLLSECTION = [home]
+
+
+
+// Открытие меню
+
 navBtn.onclick = () => {
     if (nav.classList.toggle('open')) {
-        navBtnImg.src ='../img/icons/nav-close.svg';
+        navBtnImg.src = '../img/icons/nav-close.svg';
     } else {
-        navBtnImg.src ='../img/icons/nav-open.svg';
+        navBtnImg.src = '../img/icons/nav-open.svg';
     }
 };
+
+// активное значение в меню
+
+
 
 AOS.init();
