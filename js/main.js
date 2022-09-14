63cd09a30719d6d1713cbc2b90823c30f04018df
Elizaveta Biryukova <birliza@mail.ru>
2022-09-14 23:20:58 +0200

    Removes AOS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.nav-button');
-const navBtnImg = document.querySelector('.nav-button-img');
 
 const navLink = document.querySelectorAll('.nav-link');
 const navList = document.querySelector('.nav-list');
@@ -9,20 +8,15 @@ const portfolio = document.querySelector('.project');
 const contacts = document.querySelector('.contacts');
 const SCROLLSECTION = [home, portfolio, contacts];
 
-// Смена иконок меню
-const changeIconMenu = () => {
-    if (nav.classList.toggle('open')) {
-        navBtnImg.src = '../img/icons/nav-close.svg';
-    } else {
-        navBtnImg.src = '../img/icons/nav-open.svg';
-    }
-};
+//Открытие меню
+navBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    nav.classList.toggle('open');
+});
 
-navBtn.addEventListener('click', changeIconMenu);
-
-//Закрытие меню
-navList.addEventListener('click', changeIconMenu);
-
+navList.addEventListener('click', () => {
+    nav.classList.toggle('open');
+});
 
 // Активное значение в меню
 const observer = new IntersectionObserver((enteries) => {
@@ -42,7 +36,3 @@ SCROLLSECTION.forEach((item) => {
     observer.observe(item);
 
 });
-
-
-
-AOS.init();
